d2fbf0b53e8b77c31cddd99ca4a0863e058a0fa7
Matheo Breugnot <matheo9477@gmail.com>
2024-08-24 20:44:06 +0200

    Schema book refait

--- Backend/models/Book.js
+++ Backend/models/Book.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
 
 const bookSchema = mongoose.Schema({
-    userId : String,
-    title : String,
-    author : String,
-    imageUrl : String,
-    year: Number,
-    genre: String,
-    ratings : [
+    userId: { type : String, required: true },
+    title: { type : String, required : true },
+    author: { type : String, required : true },
+    imageUrl: { type : String, required : true },
+    year: { type : Number, required : true },
+    genre: { type : String, required : true },
+    ratings: [
     {
-    userId : String,
-    grade : Number,
+    userId: { type : String, required : true},
+    grade: { type : Number, required : true},
     }
     ],
-    averageRating : Number
+    averageRating: {type : Number, required : true}
 });
 
 module.exports = mongoose.model('Book', bookSchema);
